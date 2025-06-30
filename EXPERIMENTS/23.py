0dedcb8d0c1fb5554e0316885d2ffb52f3beb671
Muhammad Aakif K F <148759553+aakif7k@users.noreply.github.com>
2025-06-30 09:17:34 +0530

    Create 23.py

---
+++ EXPERIMENTS/23.py
@@ -0,0 +1,9 @@
+import cv2
+
+img = cv2.imread('image.jpg')
+gaussian = cv2.GaussianBlur(img, (9,9), 10.0)
+unsharp = cv2.addWeighted(img, 1.5, gaussian, -0.5, 0)
+
+cv2.imshow('Unsharp Masking', unsharp)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
