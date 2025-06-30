d5c9a11ab580c45f2eec131c668252fc90bc73ce
Muhammad Aakif K F <148759553+aakif7k@users.noreply.github.com>
2025-06-30 08:29:18 +0530

    Create 1.py

---
+++ EXPERIMENTS/1.py
@@ -0,0 +1,10 @@
+# 1. Read an image in Python and convert the image to Grayscale
+
+import cv2
+
+img = cv2.imread("image.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Grayscale", gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
