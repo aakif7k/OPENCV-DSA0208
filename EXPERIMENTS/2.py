32390230f436dbc1b397eeda7e44e0ba828f7e86
Muhammad Aakif K F <148759553+aakif7k@users.noreply.github.com>
2025-06-30 08:30:17 +0530

    Create 2.py

---
+++ EXPERIMENTS/2.py
@@ -0,0 +1,10 @@
+# 2. Read an image in Python and convert the image to Blur using GaussianBlur
+
+import cv2
+
+img = cv2.imread("image.jpg")
+blur = cv2.GaussianBlur(img, (15, 15), 0)
+
+cv2.imshow("Blurred", blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
