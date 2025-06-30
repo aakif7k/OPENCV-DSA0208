2a5ab6af15f87a046ede9d63a178c3730cb2e880
Muhammad Aakif K F <148759553+aakif7k@users.noreply.github.com>
2025-06-30 09:16:45 +0530

    Create 21.py

---
+++ EXPERIMENTS/21.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('image.jpg', 0)
+
+laplacian_diagonal = np.array([[1, 1, 1],
+                                [1, -8, 1],
+                                [1, 1, 1]])
+
+sharpened = cv2.filter2D(img, -1, laplacian_diagonal)
+
+cv2.imshow('Sharpened Image', sharpened)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
