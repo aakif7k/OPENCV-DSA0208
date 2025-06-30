86dafeadf8999e2f3d0fbcf3e80a61738c410d7f
Muhammad Aakif K F <148759553+aakif7k@users.noreply.github.com>
2025-06-30 09:36:14 +0530

    Create 29_to_35.py

---
+++ EXPERIMENTS/29_to_35.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('lena.jpg', 0)
+_, binary = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+kernel = np.ones((5,5), np.uint8)
+
+erosion = cv2.erode(binary, kernel, iterations=1)
+dilation = cv2.dilate(binary, kernel, iterations=1)
+opening = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
+closing = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
+gradient = cv2.morphologyEx(binary, cv2.MORPH_GRADIENT, kernel)
+tophat = cv2.morphologyEx(binary, cv2.MORPH_TOPHAT, kernel)
+blackhat = cv2.morphologyEx(binary, cv2.MORPH_BLACKHAT, kernel)
+
+# Choose which to show
+cv2.imshow('Morph Operation1', erosion)
+cv2.imshow('Morph Operation2', dilation)
+cv2.imshow('Morph Operation3', opening)
+cv2.imshow('Morph Operation4', closing)
+cv2.imshow('Morph Operation5', tophat)
+cv2.imshow('Morph Operation6', blackhat)
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
